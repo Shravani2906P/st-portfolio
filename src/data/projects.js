470d5119beb50738a6c385ba0dcfd18bb5b8ccfc
Shravani2906P <pachkawadeshravani@gmail.com>
2025-12-24 23:05:42 +0530

    Added project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,7 @@
 export const projects = [
     {
         title: "FeedForward App",
-        image: "/public/feedforward.jpg",
+        image: "/feedforward.jpg",
         tech: ["React Native", "Node.js", "CSS", "Geomapping"],
         description:
             "A mobile app that allows users to share feedback anonymously.",
@@ -9,17 +9,17 @@ export const projects = [
         live: "https://example.com"
   },
   {
-        title: "Resume Builder App",
-        image: "../../public/eg.avif",
-        tech: ["React", "Node.js", "CSS","React", "TailwindCss"],
+        title: "Portfolio Website",
+        image: "/portfolio.png",
+        tech: ["Vite", "HTML", "CSS","Javascript"],
         description:
-          "A full-stack resume management app with ATS tracking. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        github: "https://example.com",
-        live: "https://example.com"
+          "Build a theme toggle basic Portfolio Website using Vite framework.",
+        github: "https://github.com/Shravani2906P/st-portfolio",
+        live: "https://shravaniportfolio-drab.vercel.app/"
   },
 {
         title: "FeedForward App",
-        image: "../../public/eg.avif",
+        image: "/eg.avif",
         tech: ["React Native", "Node.js", "CSS", "Geomapping"],
         description:
             "A mobile app that allows users to share feedback anonymously.",
@@ -28,7 +28,7 @@ export const projects = [
   },
   {
         title: "Resume Builder App",
-        image: "../../public/eg.avif",
+        image: "/eg.avif",
         tech: ["React", "Node.js", "CSS","React", "TailwindCss"],
         description:
           "A full-stack resume management app with ATS tracking.",
